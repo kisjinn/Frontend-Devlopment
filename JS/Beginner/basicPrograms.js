ae2e3bf3981781f7c9cff2a5d06d4cce1a5cbb49
Sakshi Jinnewar <sakshijinnewar@gmail.com>
2023-10-13 21:47:18 +0530

    Add files via upload

---
+++ JS/Beginner/basicPrograms.js
@@ -0,0 +1,10 @@
+// //Basic Tweeter input/Comment box
+// var nums=prompt("Enter tweet");
+// var len=nums.length;
+// alert("you have written "+len+"char,still"+(180-len)+" character left");
+// alert("cut down to 140 characters"+nums.slice(0,140));
+
+//DogAge Calculator
+var dogAge=prompt("Enter dog Age");
+var humanAge=(dogAge-2)* 4 + 21;
+alert(humanAge);
