3996261a396268e283fa65c869f4cf00cfb3a855
Sakshi Jinnewar <sakshijinnewar@gmail.com>
2023-11-22 23:17:26 +0530

    Add files via upload

---
+++ JS/Complere JavaScript/Functions and Methods/script.js
@@ -0,0 +1,16 @@
+//functions in JS
+function Myfunction(){
+    console.log("Sakshi Jinnewar");
+}
+
+Myfunction(); //calling above function i.e. invoking function
+
+//function helps to minimize redundancy
+
+//Function with parameter
+function printing(msg){ //msg is a parameter
+    console.log(msg);
+}
+ printing("My name is SJ"); //passing string is argument
+
+
